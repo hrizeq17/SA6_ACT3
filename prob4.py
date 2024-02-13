7b6e49a4160073edf16610b2b92b39367c77daf1
hrizeq17 <hrizeq@memphis.edu>
2024-02-12 18:19:42 -0600

    fourth _prob_done

---
+++ prob4.py
@@ -0,0 +1,6 @@
+def find_intersection(list1, list2):
+    return list(filter(lambda x: x in list2, list1))
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+intersection = find_intersection(list1, list2)
+print("Intersection:", intersection)
